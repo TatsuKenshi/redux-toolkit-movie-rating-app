de2ec31805c2939537e291c97bd51ffff1d9e9f9
TatsuKenshi <rockinbg@gmail.com>
2022-09-26 17:36:51 +0200

    adds routing, pages, components, homepage styling, axios fetch test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
                 console.log("Api response :", response);
             };
 
-            fetchMovies();
+            // fetchMovies();
         }
     }, []);
 
